22dedf986edac7429031e277f38335294287cc7a
UTR_ADP <utr160626@gmail.com>
2022-03-07 17:23:13 +0900

    enum 테스트

---
+++ src/Factory/EighthClass.java
@@ -0,0 +1,37 @@
+package Factory;
+
+public class EighthClass
+{
+    public static void main(String[] args)
+    {
+        System.out.println(Color.White);
+
+        // Todo: enum 배열 생성 및 출력
+        Color[] allColors = Color.values();
+        for (Color cl : allColors)
+            System.out.println(cl);
+
+        // Todo: 주문 관리
+        Customer[] cst = Customer.values();
+        for (Customer cl : cst)
+            System.out.println(cl);
+    }
+
+    enum Color
+    {
+        White,
+        RED,
+        GREEN,
+        BLUE,
+        BLACK
+    }
+
+    enum Customer{ // 주문 정보 - 주문 전송 - 주문 확인 - 배송 준비 - 배송중 - 배송 완료
+        OrderInfo,
+        OrderSubmit,
+        OrderCheck,
+        DeliveryReady,
+        Delivering,
+        DeliveryCompleted
+    }
+}
