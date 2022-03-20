9b77525571603781ba96883d2cf475e94abce922
UTR_ADP <utr160626@gmail.com>
2022-03-20 19:59:26 +0900

    Create Zombie extends Entity

---
+++ src/entity/monster/Zombie.java
@@ -0,0 +1,29 @@
+package entity.monster;
+
+import entity.Entity;
+
+public class Zombie extends Entity
+{
+    private static boolean attack;
+
+    Zombie(){}
+
+    public static boolean frontAttack(String view)
+    {
+        if (view == "발견")
+        {
+            System.out.println("공격한다.");
+            return attack == true;
+        }
+        else if (view == "미발견")
+        {
+            System.out.println("공격하지 않는다.");
+            return attack == false;
+        }
+        else
+        {
+            System.out.println("\"좀비는 아모고토 모른다.\"");
+            return attack == false;
+        }
+    }
+}
