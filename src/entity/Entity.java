b0f2f7276066e77ed471e86b8ef6cae7c730a931
UTR_ADP <utr160626@gmail.com>
2022-03-20 19:58:08 +0900

    Create Entity for Inheritance

---
+++ src/entity/Entity.java
@@ -0,0 +1,27 @@
+package entity;
+
+public class Entity
+{
+    private int id;
+    private int health;
+    private String name;
+//    private Skill skill;
+
+    public Entity() {}
+
+//    public Entity(int id, int health, String name, Skill skill)
+//    {
+//        this.id = id;
+//        this.health = health;
+//        this.name = name;
+//        this.skill = skill;
+//    }
+
+    // Method: 임시 생성자
+    public Entity(int id, int health, String name, Skill skill)
+    {
+        this.id = id;
+        this.health = health;
+        this.name = name;
+    }
+}
