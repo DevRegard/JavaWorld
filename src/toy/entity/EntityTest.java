b356009e5a147d18c8a82686443208edf33051fd
UTR_ADP <utr160626@gmail.com>
2022-03-22 20:42:27 +0900

    EntityTest

--- src/toy/entity/EntityTest.java
+++ src/toy/entity/EntityTest.java
@@ -1,9 +1,11 @@
 package toy.entity;
 
+import toy.entity.monster.Zombie;
+
 public class EntityTest
 {
     public static void main (String[] args)
     {
-//        Zombie zb = new Zombie();
+        Zombie zb = new Zombie(1, 100, "좀비 원");
     }
 }
