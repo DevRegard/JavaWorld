958e7548bb20e4b9eb22506f1308001ef0c234de
UTR_ADP <utr160626@gmail.com>
2022-03-23 23:34:29 +0900

    new test

---
+++ src/codingTest/GetReceiveResults.java
@@ -0,0 +1,10 @@
+package codingTest;
+
+class GetReportResults
+{
+    public int[] solution(String[] id_list, String[] report, int k)
+    {
+        int[] answer = {};
+        return answer;
+    }
+}
