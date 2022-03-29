ed8c419e5e67fc533193770a073e1469178c8b88
UTR_ADP <utr160626@gmail.com>
2022-03-29 23:59:15 +0900

    보초법

--- src/lab/search/SeqSearchSen.java
+++ src/lab/search/SeqSearchSen.java
@@ -11,10 +11,22 @@ package lab.search;
     - N은 숫자, a[i]는 맨 마지막 인덱스
   2) 반복문의 종료 판단 횟수 2회 -> 1회 줄일 수 있음
     - 이유: 원하는 값이 존재하지 않아도 보초값(마지막값)을 검색하면 자동 종료
+
 [3]
 
  */
 public class SeqSearchSen
 {
+    static int seqSearchSen_i(int[] a, int n, int key)
+    {
+        int i = 0;
+        while (true)
+        {
+            if (a[i] == key) break;
+            i++;
+        }
+
+        return i == n ? -1 : i;
+    }
 
 }
