145d13949170ec538f7872d4567063b08b35529c
DevRegard <utr160626@gmail.com>
2022-03-31 21:56:10 +0900

    보초법

---
+++ src/lab/search/BinSearch.java
@@ -0,0 +1,21 @@
+package lab.search;
+
+/*
+[1] 이진 검색: 이미 정렬된 배열에서 검색하는 알고리즘
+[2]
+  특징
+  1) 선형 검색 보다 빠름
+  2) '업 다운'
+
+  방법
+
+
+ */
+
+public class BinSearch
+{
+    static int binSearch(int[] a, int len, int key)
+    {
+        return -1;
+    }
+}
