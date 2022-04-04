766384a94b060b415a7a5a892e75cb3ad19d29fe
DevRegard <utr160626@gmail.com>
2022-04-04 20:53:55 +0900

    신체 기록 리스트: 분석 시스템 강화

--- src/lab/class_array/PhysicalList.java
+++ src/lab/class_array/PhysicalList.java
@@ -1,15 +1,40 @@
 package lab.class_array;
 
-// [1] ISSUE: 클래스 배열 -> 신체 기록 리스트 구현
+/*
+[1] ISSUE: 클래스 배열 -> 신체 기록 리스트 구현
+[2]
+  클래스 배열 (Class Array)
+  : 다양한 데이터형 조합할 수 있는 자료구조
+
+  클래스 배열 구현
+  1) 필드
+  2) 생성자
+
+  클래스 배열 사용
+    i. 클래스명[] 변수 = {
+        new 생성자명(),
+        new 생성자명(),
+        new 생성자명()
+      };
+
+  신체 기록 리스트
+  - 이름: String
+  - 신장: int
+  - 시력: Double
+
+[3]
+  i. 필드
+ ii. 생성자
+ */
 
 public class PhysicalList
 {
-    // MD-i: 필드
+    // 필드
     String name;
     int height;
     double vision;
 
-    // MD-ii: 생성자
+    // 생성자
     PhysicalList(String name, int height, double vision)
     {
         this.name = name;
@@ -17,38 +42,3 @@ public class PhysicalList
         this.vision = vision;
     }
 }
-
-/*
-[2]
-MK| 클래스 배열 (Class Array)
-    : 다양한 데이터형 조합할 수 있는 자료구조
-
-MK| How to implement Class Array
-    1. 필드
-      - 배열 클래스의 속성값
-    2. 생성자
-      - 클래스 생성 후, 생성자 통한 데이터 추가
-
-MK| How to use Class Array
-    i. 클래스명[] 변수 = {
-        new 생성자명(),
-        new 생성자명(),
-        new 생성자명()
-      };
-
-cf. How to use Class
-    i. 클래스명 변수;
-   ii. 변수 = new 생성자();
-  iii. 클래스명 변수 = new 생성자();
-
-
-MK| 신체 기록 리스트
-    - 이름: String
-    - 키: int
-    - 시력: double
-
-[3]
-  MD-i  : 필드
-  MD-ii : 생성자
-
- */
