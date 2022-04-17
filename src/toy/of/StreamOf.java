6aea2424ee44ed0b7554ed53d4e426ffc244cd5d
DevRegard <utr160626@gmail.com>
2022-04-17 21:01:39 +0900

    of() Test

--- src/toy/of/StreamOf.java
+++ src/toy/of/StreamOf.java
@@ -16,11 +16,11 @@ public class StreamOf
          리턴: 순차적으로 Stream 포함하는 단일 특정 요소
          */
 
-        // MK: 기본 예제
+        // Mk: 기본 예제
         Stream stream = Stream.of("김두한", "5000억", "프로젝트");
         stream.forEach(System.out::println);
 
-        // MK: Stream 객체의 of()를 통한 String 변수 할당
+        // Mk: Stream 객체의 of()를 통한 String 변수 할당
         String test01 = "테스트 1";
         String test02 = "테스트 2";
         String test03 = "테스트 3";
@@ -28,26 +28,29 @@ public class StreamOf
         Stream varStream = Stream.of(test01, test02, test03);
         varStream.forEach(System.out::println);
 
-        // MK: Stream 이용한 Array 배열 할당 [String]
-        String[] arrString = {"100억", "500억", "1000억"};
+        // Mk: Stream 이용한 Array 배열 할당 [String]
+        String[] arrString = {"100억", "500억", "1000억", "5000억"};
 
-        Stream<String> arrStream = Arrays.stream(arrString);
+        Stream<String> arrStream = Arrays.stream(arrString); // 스트림 객체 <- Array.stream(배열)
 
         arrStream.forEach(str -> System.out.print(str + " "));
 
-        // MK: Stream.of() 를 이용한 Array 배열 할당 [String]
-        Stream ofStream = Stream.of("100억", "500억", "1000억");
+        // Mk: Stream.of() 를 이용한 Array 배열 할당 [String]
+        Stream ofStream = Stream.of("100억", "500억", "1000억"); //
+        Stream ofStream2 = Stream.of(arrString); // of() 을 통해 매개변수의 요소들을 순차적으로 단일 요소로 변환
 
         ofStream.forEach(str -> System.out.print(str + " "));
+        ofStream2.forEach(str -> System.out.print(str + " "));
 
-        // MK: Stream 이용한 Array 배열 할당 [int]
+        // Mk: Array vs of() 차이점
+        // 1: Stream 이용한 Array 배열 할당 [int]
         int[] arrInt = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
         IntStream intStream = Arrays.stream(arrInt);
 //        intStream.forEach(str -> System.out.print(str + " "));
 
-        // MK: Stream.of() 를 이용한 Array 배열 할당 [int] // ISSUE
-        Stream<int[]> arrIntStream = Stream.of(arrInt);
+        // 2: Stream.of() 를 이용한 Array 배열 할당 [int] ISSUE: Different return types
+        Stream<int[]> arrIntStream = Stream.of(arrInt); // int 안되는건가?
         arrIntStream.forEach(str -> System.out.print(str + " "));
     }
 }
