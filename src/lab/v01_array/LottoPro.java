412919f8c59f7e89e5ca7ec5c2ab100171a57299
DevRegard <utr160626@gmail.com>
2022-04-19 20:27:25 +0900

    연금복권 번호 추천 프로그램: 시간별 랜덤 시드

--- src/lab/v01_array/LottoPro.java
+++ src/lab/v01_array/LottoPro.java
@@ -5,6 +5,8 @@ import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Random;
 
+import static java.lang.Math.pow;
+
 public class LottoPro
 {
     // MD: 로또 기본 원리 테스트
@@ -15,13 +17,13 @@ public class LottoPro
 
         for (int i = 0; i < row.length; i++)
         {
-            int x = rand.nextInt(10) + 1; // 1 부터 10
+            int x = rand.nextInt(9) + 1; // 1 부터 9
             row[i] += x;
             System.out.print(row[i] + " ");
         }
     }
 
-    // MD: 랜덤 시간 테스트
+    // MD: 랜덤 시간 테스트(곱셈)
     static int testTimeRandom()
     {
         LocalTime time = LocalTime.now();
@@ -30,12 +32,29 @@ public class LottoPro
         int minute = time.getMinute();
         int second = time.getSecond();
 
-        int seedTime = hour + minute + second;
+        int seedTime = hour * minute * second;
 
-        System.out.println(hour + " + " + minute + " + " + second);
-        System.out.println(seedTime);
+        System.out.println("현재 시분초: " + hour + " * " + minute + " * " + second);
+        System.out.println("현재 시분초 곱하기: " + seedTime);
 
         return seedTime;
+    }
+
+ // MD: 랜덤 시간 테스트(제곱)
+    static double testTimeRandomPow()
+    {
+        LocalTime time = LocalTime.now();
+
+        double hourPow = time.getHour();
+        double minutePow = time.getMinute();
+        double secondPow = time.getSecond();
+
+        double seedTimePow = pow(hourPow, minutePow);
+
+        System.out.println("현재 시분초: " + hourPow + " ** " + minutePow + " ** " + secondPow);
+        System.out.println("현재 시분초 제곱: " + seedTimePow);
+
+        return seedTimePow;
     }
 
     // MD: 랜덤 날짜 테스트
@@ -52,8 +71,12 @@ public class LottoPro
     // MD: 로또 프로그램 프로토타입
     static void lottoProType()
     {
-        Random randDate = new Random(testTimeRandom());
+        Random randDate = new Random(testTimeRandom()); // 곱하기 시드
+        Random randDatePow = new Random((long) testTimeRandomPow()); // 제곱 시드
+
         testLotto(randDate);
+        System.out.println("\n");
+        testLotto(randDatePow);
         /*
           i. 랜덤으로 생성할 때마다 중복 체크
           ii. 서로 일치하는 숫자가 1개라도 있으면 다시 추첨
